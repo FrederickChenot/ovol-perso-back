f83d0cd1b78674446bf96bc8ec6396ae6ba7769c
Adrien35240 <adriendev35240@gmail.com>
2022-02-28 13:16:15 +0100

    fix(doc): update gitignore

---
+++ docs/test.sql
@@ -0,0 +1,27 @@
+SELECT * FROM hiking
+JOIN img_hiking ON img_hiking."idHiking" = hiking."id"
+WHERE hiking."id" = 1
+
+-- cette requete fonctionne
+SELECT * FROM "hiking"
+JOIN "img_hiking" ON img_hiking."idHiking" = "hiking"."id"
+JOIN "lift-off" ON "hiking"."lift-off_id" = "lift-off"."id"
+JOIN "img_lift-off" ON "lift-off"."id" = "img_lift-off"."idLiftOff"
+JOIN "lift-off_has_landing" ON "lift-off_has_landing"."lift-off_id" = "lift-off"."id"
+JOIN "landing" ON "lift-off_has_landing"."landing_id" = "landing"."id"
+WHERE hiking."id" = 1
+
+
+
+SELECT DISTINCT *,array_agg("landing") AS landings FROM "lift-off"
+JOIN "lift-off_has_landing" ON "lift-off_has_landing"."lift-off_id" = "lift-off"."id"
+JOIN "landing" ON "lift-off_has_landing"."landing_id" = "landing"."id"
+group by "lift-off"."id","lift-off_has_landing"."id","landing"."id"
+
+SELECT * FROM "hiking"
+JOIN "img_hiking" ON img_hiking."idHiking" = "hiking"."id"
+JOIN "lift-off" ON "hiking"."lift-off_id" = "lift-off"."id"
+JOIN "img_lift-off" ON "lift-off"."id" = "img_lift-off"."idLiftOff"
+WHERE hiking."id" = 1
+
+SELECT json_agg(row_to_json("landing")) FROM landing
