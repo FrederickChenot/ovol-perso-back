3962652b0d9d5e026e1b7a8522ad6845714b56b5
Oliv74 <olivier.reboul@gmail.com>
2022-02-28 11:01:25 +0100

    fix(seeding.sql): missing ; at end image seeding

--- app/migrations/deploy/2-seeding.sql
+++ app/migrations/deploy/2-seeding.sql
@@ -40,14 +40,14 @@ VALUES
 
 INSERT INTO "img_hiking"("title", "url", "idHiking")
 VALUES
-    ('Photo Semnoz', 'url”: “https://live.staticflickr.com/65535/50932308243_a19f408059_b.jpg', 1)
+    ('Photo Semnoz', 'url”: “https://live.staticflickr.com/65535/50932308243_a19f408059_b.jpg', 1);
 
 INSERT INTO "img_landing"("title", "url", "idLanding")
 VALUES
-    ('Photo atero Semnoz', 'https://www.lofficiel.net/img/guide/2917-atterissage-bois-du-bouchet-karine-payot-fiche.jpg', 1)
+    ('Photo atero Semnoz', 'https://www.lofficiel.net/img/guide/2917-atterissage-bois-du-bouchet-karine-payot-fiche.jpg', 1);
 
 INSERT INTO "img_lift-off"("title", "url", "idLiftOff")
 VALUES
-    ('Photo deco Semnoz', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Parapente_-_162.jpg/800px-Parapente_-_162.jpg?2015110313514', 1)
+    ('Photo deco Semnoz', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Parapente_-_162.jpg/800px-Parapente_-_162.jpg?2015110313514', 1);
 
 COMMIT;
