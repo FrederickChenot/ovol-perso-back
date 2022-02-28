4da0c3c341fffeff38f6ec011b2fea26fd92a3d1
Adrien35240 <adriendev35240@gmail.com>
2022-02-28 10:12:27 +0100

    feat(lift-off): add route

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const hikingController = require('../controllers/hiking.controllers');
-
-router.route('/api/hiking').get(hikingController.getAll);
+const liftOffController = require('../controllers/lift-off.controllers');
 
 router.route('/api/hiking/:id').get(hikingController.getOne);
+router.route('/api/hiking').get(hikingController.getAll);
+
+router.route('/api/lift-off/:id').get(liftOffController.getOne);
+router.route('/api/lift-off').get(liftOffController.getAll);
 
 router.use((_req, res) => {
   res.json({ message: 'page not found' });
