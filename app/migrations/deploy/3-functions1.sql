12f03414bab9c8051758effffdc50c1b415b404c
Adrien35240 <adriendev35240@gmail.com>
2022-03-01 13:51:33 +0100

    feat(hiking): id landing in hiking

--- app/migrations/deploy/3-functions1.sql
+++ app/migrations/deploy/3-functions1.sql
@@ -45,7 +45,7 @@ CREATE OR REPLACE FUNCTION getLiftOff(int) RETURNS TABLE (
 	"favorable-wind" text[],
 	"unfavorable-wind" text[],
 	"altitude" int,
-	"landings" json[],
+	"idLandings" int[],
 	"photo_lift-off" json[]
 ) AS $$
 SELECT "lift-off"."id",
@@ -59,7 +59,7 @@ SELECT "lift-off"."id",
 	"lift-off"."favorable-wind",
 	"lift-off"."unfavorable-wind",
 	"lift-off"."altitude",
-	array_agg(row_to_json("landing")) AS "landings",
+	array_agg("landing"."id") AS "idLandings",
 	array_agg(row_to_json("img_lift-off")) AS "photo_lift-off" FROM "lift-off"
 JOIN "img_lift-off" 
 	ON "img_lift-off"."idLiftOff" = "lift-off"."id"
--- app/models/hiking.js
+++ app/models/hiking.js
@@ -9,7 +9,7 @@ module.exports = {
   async findOne(idHiking) {
     let result = await client.query('SELECT * FROM getOneHiking($1)', [idHiking]);
     const liftOff = await client.query(`SELECT * FROM getLiftOff($1)`, [result.rows[0]['lift-off_id']])
-    result.rows[0].liftOff = liftOff.rows
+    result.rows[0].idLandings = liftOff.rows[0].idLandings
     return result.rows;
   },
 };
