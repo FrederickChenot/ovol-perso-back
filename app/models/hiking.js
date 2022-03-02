eb51cf985601452eba1ab54377ce2e968c7712bb
Oliv74 <olivier.reboul@gmail.com>
2022-03-02 11:18:08 +0100

    feat: controller datamapper and test

--- app/models/hiking.js
+++ app/models/hiking.js
@@ -12,4 +12,67 @@ module.exports = {
     result.rows[0].idLandings = liftOff.rows[0].idLandings;
     return result.rows;
   },
+
+  async creatOne(
+    name,
+    imgCard,
+    mountain,
+    resume,
+    keyStage,
+    startingPoint,
+    hikingPlan,
+    positiveElevation,
+    negativeElevation,
+    overallLength,
+    landType,
+    ignCardReference,
+    hightPoint,
+    lowPoint,
+    difficulty,
+    userId,
+    liftOffId,
+  ) {
+    const query = {
+      text: `INSERT INTO "hiking"
+            ("name",
+            "img_card",
+            "mountain",
+            "resume",
+            "key_stage",
+            "starting_point",
+            "hiking_plan",
+            "positive_elevation",
+            "negative_elevation",
+            "overall_length",
+            "land_type",
+            "ign_card_reference",
+            "hight_point",
+            "low_point",
+            "difficulty",
+            "user_id",
+            "lift-off_id")
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+            RETURNING id`,
+      values: [
+        name,
+        imgCard,
+        mountain,
+        resume,
+        keyStage,
+        startingPoint,
+        hikingPlan,
+        positiveElevation,
+        negativeElevation,
+        overallLength,
+        landType,
+        ignCardReference,
+        hightPoint,
+        lowPoint,
+        difficulty,
+        userId,
+        liftOffId],
+    };
+    const result = await client.query(query);
+    return result.rows;
+  },
 };
