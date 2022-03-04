fce9f826992231e14bcfcf1afed0e16dce71502f
Oliv74 <olivier.reboul@gmail.com>
2022-03-04 09:27:30 +0100

    fix: add a landing for the hiking 1 and scrit npm script restBD

--- app/migrations/deploy/2-seeding.sql
+++ app/migrations/deploy/2-seeding.sql
@@ -189,14 +189,26 @@ VALUES
     6.43168,
     ARRAY ['O'],
     null,
-    926);
+    926),
+
+     ('Atéro Gruffy',
+    'Herbe',
+    'Attention des arbres et des vaches partout l''enfer.',
+    null,
+    'atéro non officiel',
+    45.792968,
+    6.058227,
+    ARRAY ['O', 'OE'],
+    null,
+    580)    ;
 
 INSERT INTO "liftOff_has_landing"("liftOff_id","landing_id")
 VALUES
     (1,1),
     (2,2),
     (3,3),
-    (4,4);
+    (4,4),
+    (1,5);
 
 INSERT INTO "img_hiking"("title", "url", "idHiking")
 VALUES
@@ -216,7 +228,8 @@ VALUES
     ('Photo atero Jura2', 'https://c.pxhere.com/photos/f6/60/paratrooper_jump_double_baptism_landing-1104626.jpg!d', 2),
     ('Photo atero Vailly2', 'https://lh5.googleusercontent.com/p/AF1QipNPpNs4005pLpzwVf9k0R6HlQB-ngAWU5bCqGFr=w426-h240-k-no', 3),
     ('Photo atero Vailly', 'https://lh5.googleusercontent.com/p/AF1QipNPpNs4005pLpzwVf9k0R6HlQB-ngAWU5bCqGFr=w426-h240-k-no', 3),
-    ('Photo atero grand Bornand', 'https://c.pxhere.com/photos/65/b3/skydiving_parachute_parachutist_sport_sunset_landing-1392169.jpg!d', 4);
+    ('Photo atero grand Bornand', 'https://c.pxhere.com/photos/65/b3/skydiving_parachute_parachutist_sport_sunset_landing-1392169.jpg!d', 4),
+    ('Photo atero Guffy', 'https://c.pxhere.com/photos/65/b3/skydiving_parachute_parachutist_sport_sunset_landing-1392169.jpg!d', 5);
 
 INSERT INTO "img_liftOff"("title", "url", "idLiftOff")
 VALUES
