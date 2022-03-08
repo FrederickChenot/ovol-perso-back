83a7d9ca5e0327af0317a314d2a60633dda557c1
oliv42 <olivier.reboul@gmail.com>
2022-03-08 17:40:08 +0100

    test: test errur status

--- app/helpers/apiController.js
+++ app/helpers/apiController.js
@@ -8,6 +8,6 @@ module.exports = (controller) => async (req, res, next) => {
     await controller(req, res, next);
   } catch (err) {
     console.error(err);
-    res.status(500).send(err);
+    res.status(505).send(err);
   }
 };
