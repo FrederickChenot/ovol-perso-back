7ba6f505c3a5fa97b8a7f6557b8515d064659261
Adrien35240 <adriendev35240@gmail.com>
2022-03-10 11:39:20 +0100

    feat(seeding): add photo to cloudinary

--- app/routes/landing.js
+++ app/routes/landing.js
@@ -57,8 +57,7 @@ router.route('/')
       "favorableWind":"O",
       "unfavorableWind":"O",
       "altitude":2313,
-       "photo_landing": "'name': 'update photo  test2', 'url': 'https://idata.over-blog.com/2/08/31/84/gifs/cockpit-avion.jpg', 'name': 'update photo rasdfsdfndo test', 'url': 'https://idata.over-blog.com/image.jpg', 'name': 'Oliv3', 'url': 'https://idata.over-blog.com/Olivier_url.jpg'"
-}
+       "photo_landing": "'name': 'update photo  test2', 'url': 'https://idata.over-blog.com/2/08/31/84/gifs/cockpit-avion.jpg', 'name': 'update photo rasdfsdfndo test', 'url': 'https://idata.over-blog.com/image.jpg', 'name': 'Oliv3', 'url': 'https://idata.over-blog.com/Olivier_url.jpg'"}
    *@example response - 200 - Return json with the new id of the landing
   {
     "id": 5
