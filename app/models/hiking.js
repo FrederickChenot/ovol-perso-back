b1bfea6c74b972df2ad7dc7fbbe4fe5f50d3cbf3
oliv42 <olivier.reboul@gmail.com>
2022-03-11 15:59:04 +0100

    test: hiking12

--- app/models/hiking.js
+++ app/models/hiking.js
@@ -108,7 +108,7 @@ module.exports = function datamapper() {
             "user_id",
             "liftOff_id")
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
-            RETURNING id`,
+            RETURNING *`,
       values: [
         data.name,
         data.img_card,
